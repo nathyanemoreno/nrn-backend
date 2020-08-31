66dd509cf23395c7068975cc7fd002bad461bb94
Nathyane Moreno <nathyane.moreno@gmail.com>
2020-08-31 15:04:49 -0300

    Function to get slice from dataframe added.

--- app.py
+++ app.py
@@ -5,20 +5,25 @@ from ast import literal_eval
 
 app = Flask(__name__)
 
-n_column = 15
-interval = 25
+
+# c = it.combinations(range(1,interval+1),n_column)
+
 @app.route('/')
 def index():
     return "<h1>Sistema de combinações</h1>"
 @app.route('/listar',methods=['GET', 'POST'] )
 def listar():
     f = literal_eval(request.args.get('f', None))
-    
-    rg = len(f)
-    c = it.combinations(range(1,interval+1),n_column)
-    filtered = jsonify(list(it.filterfalse(lambda x: x[0:rg] != f, c)))
-    return filtered
-
+    start = literal_eval(request.args.get('start', None))
+    end =  literal_eval(request.args.get('end', None))
+    n_column = 15
+    interval = 25
+    if (f != None):
+        rg = len(f)
+        filtered = jsonify(list(it.filterfalse(lambda x: x[0:rg] != f, it.combinations(range(1,interval+1),n_column))))
+        return filtered
+    else:
+        return jsonify (list(it.islice(it.combinations(range(1,interval+1),n_column), start, end)))
 
 if __name__ =="__main__":
-    app.run(debug=True)
+    app.run()
